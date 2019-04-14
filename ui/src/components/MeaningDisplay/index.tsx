a4a7af9317a565e7b91a24adf88bf26416893eb7
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2019-04-14 14:26:19 +0200

    add animation.

--- ui/src/components/MeaningDisplay/index.tsx
+++ ui/src/components/MeaningDisplay/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Spring } from 'react-spring/renderprops';
 
 import styles from './styles.module.scss';
 
@@ -8,8 +9,27 @@ interface Props {
 
 const MeaningDisplay = (props: Props) => {
   return (
-    <div className={styles.word_display}>
-      <h1>{props.englishWord}</h1>
+    <div className={styles.meaning_display}>
+      <Spring
+        from={{ x: 0 }}
+        to={{ x: 100 }}
+        config={{ duration: 1100 }}
+        key={`${props.englishWord}-1`}
+      >
+        {p => (
+          <svg>
+            <line x1="0" y1="0" x2={`${p.x}%`} y2="0" stroke="#FF509D" strokeWidth="5" />
+          </svg>
+        )}
+      </Spring>
+      <Spring
+        from={{ opacity: 0 }}
+        to={{ opacity: 1 }}
+        delay={1000}
+        key={`${props.englishWord}-2`}
+      >
+        {p => <h1 style={p}>{props.englishWord}</h1>}
+      </Spring>
     </div>
   );
 };
